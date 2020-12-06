fba1bd237fac9ea1fae62488e4ece68568a978ac
sek3951 <dnjswp39511@gmail.com>
2020-12-06 17:04:18 +0900

    test

--- Shuffle.cc
+++ Shuffle.cc
@@ -1,14 +1,13 @@
 #include <cstdlib>
 #include <time.h>
 #include "common.h"
-
 void Shuffle(int *deck){
         for(int i = 1; i <= 52; i++)
                 deck[i-1] = i;
         int tmp1;
         for(int i = 0; i < 52; i++){
                 srand(time(NULL));
-                int random = rand() % 52;
+                int random = rand() %(52-i)+i;
                 tmp1 = deck[i];
                 deck[i] = deck[random];
                 deck[random] = tmp1;
