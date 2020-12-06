fcc5a0f4b93ff1364341ac106d61139eea76fa0c
seok0928 <j_h_suk0928@naver.com>
2020-12-06 15:40:02 +0900

    card shuffle function

---
+++ shuffle.cc
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <time.h>
+#include "common.h"
+void Shuffle(int *deck){
+        for(int i = 1; i <= 52; i++)
+                deck[i-1] = i;
+        int tmp1;
+        for(int i = 0; i < 52; i++){
+                srand(time(NULL));
+                int random = rand() % 52;
+                tmp1 = deck[i];
+                deck[i] = deck[random];
+                deck[random] = tmp1;
+        }
+}
+
