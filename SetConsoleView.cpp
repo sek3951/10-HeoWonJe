b4d7f83a3a8d0df10a3c092161928df41f4c6c31
kwlsgur <kwlsgur@naver.com>
2020-12-08 00:15:28 +0900

    SetConsoleView

---
+++ SetConsoleView.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+#include<windows.h>
+#include<conio.h>
+#include<ctime>
+#include<iostream>
+#include "common.h"
+
+//콘솔 창의 크기와 제목을 지정하는 함수
+void SetConsoleView()
+{
+	system("mode con:cols=100 lines=25");
+	system("Final Project - 10Team");
+}
