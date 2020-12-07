e864789b3b41375f2634f2e197fc057555dce6f3
kwlsgur <kwlsgur@naver.com>
2020-12-08 03:58:40 +0900

    leaderboard - DrawGameOver.cpp

--- DrawGameOver.cpp
+++ DrawGameOver.cpp
@@ -8,20 +8,25 @@
 using namespace std;
 
 //(v2.0) 충돌 했을때 게임오버 그려줌
-void DrawGameOver(const int score)
+void DrawGameOver(const int score, const int leader_board[])
 {
 	system("cls");
-	int x = 18;
-	int y = 8;
+	int x = 16;
+	int y = 3;
 	GoToXY(x, y);
-	cout << "===========================";
+	cout << "=================================";
 	GoToXY(x, y + 1);
-	cout << "======G A M E O V E R======";
+	cout << "======== G A M E O V E R ========";
 	GoToXY(x, y + 2);
-	cout << "===========================";
-	GoToXY(x, y + 5);
-	cout << "SCORE : " << score;
-
-	cout << "\n\n\n\n\n\n\n\n\n";
+	cout << "=================================";
+	GoToXY(x, y + 4);
+	cout << "YOUR SCORE : " << score;
+	GoToXY(x, y + 7);
+	cout << "===== L E A D E R B O A R D =====";
+	for (int i = 0; i < 5; i++) {
+		GoToXY(x, y + 9 + i);
+		cout << "RANK " << i + 1 << " : " << leader_board[i];
+	}
+	cout << "\n\n\n\n\n\n";
 	system("pause");
 }
