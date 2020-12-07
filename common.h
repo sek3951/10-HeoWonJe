3633f9be501cad7ff39aa94c0fda227cc6199e11
kwlsgur <kwlsgur@naver.com>
2020-12-08 00:17:19 +0900

    common.h

---
+++ common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define DINO_BOTTOM_Y 12
+#define TREE_BOTTOM_Y 20
+#define TREE_BOTTOM_X 45
+
+void SetConsoleView();
+void GoToXY(int x, int y);
+int GetKeyDown();
+void DrawDino(int dinoY);
+void DrawTree(int treeX);
+void DrawGameOver(const int score);
+bool IsCollision(const int treeX, const int dinoY);
